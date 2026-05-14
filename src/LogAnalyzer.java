cb7c29f31436df74507c260c65b819d33517d13f
luis <luisfelipe@feyh.com.br>
2026-05-13 23:57:18 -0300

    Implement file choosing

--- src/LogAnalyzer.java
+++ src/LogAnalyzer.java
@@ -3,8 +3,16 @@ import java.io.FileReader;
 
 public class LogAnalyzer {
   public static void main(String[] args) throws Exception {
-    for(String arg : args) {
-      if(arg.equals("--print-all")) {
+    String logfile;
+    for(int i = 0; i < args.length; i++) {
+      if (i + 1 < args.length) {
+        if (args[i].equals("--file")) {
+          logfile = args[i + 1];
+          System.out.println(logfile);
+        }
+      }
+
+      if(args[i].equals("--print-all")) {
         try (BufferedReader reader = new BufferedReader(new FileReader("latest.log"))) {
           String line;
           while((line = reader.readLine()) != null) {
