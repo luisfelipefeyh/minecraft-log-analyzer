75a42ba76e3f819867b978a58051933375b158da
luis <luisfelipe@feyh.com.br>
2026-05-14 00:12:25 -0300

    Fix file argument parsing errors plus code cleanup

--- src/LogAnalyzer.java
+++ src/LogAnalyzer.java
@@ -3,19 +3,19 @@ import java.io.FileReader;
 
 public class LogAnalyzer {
   public static void main(String[] args) throws Exception {
-    String logfile;
-    for(int i = 0; i < args.length; i++) {
-      if (i + 1 < args.length) {
-        if (args[i].equals("--file")) {
+    String logfile = "latest.log";
+    for (int i = 0; i < args.length; i++) {
+      if (args[i].equals("--file")) {
+        if (i + 1 < args.length) {
           logfile = args[i + 1];
           System.out.println(logfile);
+          i++;
         }
       }
-
-      if(args[i].equals("--print-all")) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("latest.log"))) {
+      if (args[i].equals("--print-all")) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(logfile))) {
           String line;
-          while((line = reader.readLine()) != null) {
+          while ((line = reader.readLine()) != null) {
             System.out.println(line); 
           }
         }
